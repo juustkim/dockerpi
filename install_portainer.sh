2d1ba76b3ae5b8f9832c3fa4b6ef7c91859840ab
juustkim <kimmo@juustovaara.fi>
2022-02-01 15:07:24 +0300

    updates

--- install_portainer.sh
+++ install_portainer.sh
@@ -17,6 +17,17 @@ function check_internet() {
 
 check_internet
 
+sudo docker network create \
+  --driver=bridge \
+  --subnet=172.20.0.0/16 \
+  --gateway=172.20.0.1 \
+  frontend
+
+sudo docker network create \
+  --driver=bridge \
+  --subnet=172.20.0.0/16
+  backend
+
 sudo docker pull portainer/portainer-ce:latest || error "Failed to pull latest Portainer docker image!"
-sudo docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest || error "Failed to run Portainer docker image!"
+sudo docker run -d -p 9000:9000 -p 9443:9443 --network=frontend --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /container/portainer:/data portainer/portainer-ce:latest || error "Failed to run Portainer docker image!"
 
