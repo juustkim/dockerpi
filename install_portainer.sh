a43494badf16f67db0f633bc22d4ebcfe6c06908
juustkim <kimmo@juustovaara.fi>
2022-02-18 20:38:05 +0300

    Switched poratiner to use volume
    Switched to use volume

--- install_portainer.sh
+++ install_portainer.sh
@@ -29,5 +29,5 @@ sudo docker network create \
   backend
 
 sudo docker pull portainer/portainer-ce:latest || error "Failed to pull latest Portainer docker image!"
-sudo docker run -d -p 9000:9000 -p 9443:9443 --network=frontend --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /container/portainer:/data portainer/portainer-ce:latest || error "Failed to run Portainer docker image!"
+sudo docker run -d -p 9000:9000 -p 9443:9443 --network=frontend --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest || error "Failed to run Portainer docker image!"
 
