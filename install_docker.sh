1fe0b33e03f7cffe9ac2a542682e3841cad9c2de
juustkim <kimmo@juustovaara.fi>
2022-02-21 14:59:47 +0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -17,6 +17,8 @@ function check_internet() {
 
 check_internet
 
+sudo apt update && sudo apt upgrade -y
+
 curl -sSL https://get.docker.com | sh || error "Failed to install Docker."
 sudo usermod -aG docker $USER || error "Failed to add user to the Docker usergroup."
 echo "Remember to logoff/reboot for the changes to take effect."
