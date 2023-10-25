63f3aa45b0b841744443e68ee11d64b9b986239c
Kimmo Juustovaara <kimmo@LAN.OSTBERG.TK>
2023-10-25 10:56:29 +0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -17,9 +17,29 @@ function check_internet() {
 
 check_internet
 
-sudo apt update && sudo apt upgrade -y
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-curl -sSL https://get.docker.com | sh || error "Failed to install Docker."
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install Docker
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 sudo usermod -aG docker $USER || error "Failed to add user to the Docker usergroup."
-echo "Remember to logoff/reboot for the changes to take effect."
-sudo reboot now
+
+newgrp docker
+
+sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+sudo chmod g+rwx "$HOME/.docker" -R
+
+sudo mkdir /docker
+sudo chown "$USER":"$USER" /docker -R
