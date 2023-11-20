cc6cc05df47c1476a4b8892fff7094b0b7d5f0b2
Kimmo Juustovaara <kimmo@LAN.OSTBERG.TK>
2023-11-20 13:23:59 +0200

    Update install_portainer.sh

--- install_portainer.sh
+++ install_portainer.sh
@@ -28,6 +28,8 @@ sudo docker network create \
   --subnet=172.21.0.0/16 \
   backend
 
+sudo mkdir -p /docker/container/portainer
+
 sudo docker pull portainer/portainer-ce:latest || error "Failed to pull latest Portainer docker image!"
 sudo docker run -d -p 9000:9000 -p 9443:9443 --network=frontend --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /docker/container/portainer:/data portainer/portainer-ce:latest || error "Failed to run Portainer docker image!"
 
